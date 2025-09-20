b21815ce091fd992a1aa7f79f4f0566293bc593e
Taha_Afzali <elfo.dp8787@gmail.com>
2025-09-21 01:30:39 +0330

    part10

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ const account1 = {
 
   movementsDates: [
     '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2013-12-23T07:42:02.383Z',
+    '2025-09-14T09:15:04.904Z',
+    '2025-09-18T10:17:24.185Z',
+    '2025-09-19T14:11:59.604Z',
+    '2025-09-21T17:01:17.194Z',
+    '2025-09-20T23:36:17.929Z',
+    '2025-09-20T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -80,6 +80,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatMovmentDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return 'ToDay';
+  if (daysPassed === 1) return 'YesterDay';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDay()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
 
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -87,14 +103,10 @@ const displayMovements = function (acc, sort = false) {
   const movs = sort
     ? acc.movements.slice().sort((a, b) => a - b)
     : acc.movements;
-
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDay()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovmentDate(date);
 
     const html = `
       <div class="movements__row">
@@ -388,8 +400,7 @@ console.log(new Date(account1.movementsDates[0]));
 console.log(new Date(2037, 10, 19, 15, 23, 5));
 console.log(new Date(0));
 console.log(new Date(3 * 24 * 60 * 60 * 1000));
-*/
-const feuture = new Date(2037, 10, 19, 15, 23);
+
 console.log(feuture);
 console.log(feuture.getFullYear());
 console.log(feuture.getMonth());
@@ -404,3 +415,10 @@ console.log(new Date(2142244380000));
 console.log(Date.now());
 feuture.setFullYear(2040);
 console.log(feuture);
+
+const feuture = new Date(2037, 10, 19, 15, 23);
+console.log(+feuture);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
+*/
